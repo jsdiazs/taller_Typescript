5017b9d3fdaca66eafc707e34a3eaffb3ce0f22a
Jhoan Diaz <js.diazs@uniandes.edu.co>
2020-10-26 01:28:08 -0500

    Typescript

---
+++ dataStudent.ts
@@ -0,0 +1,5 @@
+import { Student } from './student.js';
+
+export const dataStudent = [
+    new Student(201918961, 1000033867,19, 'calle falsa', 3015058820)
+  ]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Rubby Casallas", 4),
+    new Course("Futbol 1", "German Bravo", 3),
+    new Course("Diseño de Algoritmos", "Rodrigo Cardozo", 3),
+    new Course("Estructuras de Datos", "Yesid D", 3),
+    new Course("Futbol 2", "James R", 2)
+];
